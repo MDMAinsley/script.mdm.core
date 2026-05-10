9be0aa6308553f28c357cb4d848b1d1b29cc4a67
MDMAinsley <lewis.james.short@gmail.com>
2026-05-10 17:38:12 +0100

    v0.0.16

--- resources/lib/mdmcore/settings.py
+++ resources/lib/mdmcore/settings.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import xbmc
 import xbmcaddon
 
-from .constants import CORE_ADDON_ID
-from .logger import exception
+from .constants import CORE_ADDON_ID, LOG_PREFIX
+
+
+def _log_settings_error(message):
+    xbmc.log("{} {}".format(LOG_PREFIX, message), xbmc.LOGERROR)
 
 
 def get_addon(addon_id=CORE_ADDON_ID):
@@ -15,34 +19,31 @@ def get_string(setting_id, addon_id=CORE_ADDON_ID, default=""):
         value = get_addon(addon_id).getSetting(setting_id)
         return value if value != "" else default
     except Exception:
-        exception("Failed to read string setting: {}".format(setting_id))
+        _log_settings_error("Failed to read string setting: {}".format(setting_id))
         return default
 
 
 def get_bool(setting_id, addon_id=CORE_ADDON_ID, default=False):
     try:
-        value = get_addon(addon_id).getSettingBool(setting_id)
-        return value
+        return get_addon(addon_id).getSettingBool(setting_id)
     except Exception:
-        exception("Failed to read bool setting: {}".format(setting_id))
+        _log_settings_error("Failed to read bool setting: {}".format(setting_id))
         return default
 
 
 def get_int(setting_id, addon_id=CORE_ADDON_ID, default=0):
     try:
-        value = get_addon(addon_id).getSettingInt(setting_id)
-        return value
+        return get_addon(addon_id).getSettingInt(setting_id)
     except Exception:
-        exception("Failed to read int setting: {}".format(setting_id))
+        _log_settings_error("Failed to read int setting: {}".format(setting_id))
         return default
 
 
 def get_float(setting_id, addon_id=CORE_ADDON_ID, default=0.0):
     try:
-        value = get_addon(addon_id).getSettingNumber(setting_id)
-        return value
+        return get_addon(addon_id).getSettingNumber(setting_id)
     except Exception:
-        exception("Failed to read float setting: {}".format(setting_id))
+        _log_settings_error("Failed to read float setting: {}".format(setting_id))
         return default
 
 
@@ -51,7 +52,7 @@ def set_string(setting_id, value, addon_id=CORE_ADDON_ID):
         get_addon(addon_id).setSetting(setting_id, str(value))
         return True
     except Exception:
-        exception("Failed to write string setting: {}".format(setting_id))
+        _log_settings_error("Failed to write string setting: {}".format(setting_id))
         return False
 
 
@@ -60,7 +61,7 @@ def set_bool(setting_id, value, addon_id=CORE_ADDON_ID):
         get_addon(addon_id).setSettingBool(setting_id, bool(value))
         return True
     except Exception:
-        exception("Failed to write bool setting: {}".format(setting_id))
+        _log_settings_error("Failed to write bool setting: {}".format(setting_id))
         return False
 
 
@@ -69,7 +70,7 @@ def set_int(setting_id, value, addon_id=CORE_ADDON_ID):
         get_addon(addon_id).setSettingInt(setting_id, int(value))
         return True
     except Exception:
-        exception("Failed to write int setting: {}".format(setting_id))
+        _log_settings_error("Failed to write int setting: {}".format(setting_id))
         return False
 
 
@@ -78,5 +79,5 @@ def set_float(setting_id, value, addon_id=CORE_ADDON_ID):
         get_addon(addon_id).setSettingNumber(setting_id, float(value))
         return True
     except Exception:
-        exception("Failed to write float setting: {}".format(setting_id))
+        _log_settings_error("Failed to write float setting: {}".format(setting_id))
         return False
