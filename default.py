4f779f97c1a0f09faa22b1b01c0383979b2b7017
MDMAinsley <lewis.james.short@gmail.com>
2026-05-10 15:46:26 +0100

    v0.0.9

--- default.py
+++ default.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from resources.lib.mdmcore.cache import set_cache, get_cache
-
 from resources.lib.mdmcore.logger import info
 
 
 def main():
     info("MDM Core loaded successfully!")
-
-    set_cache("test_cache", {"working": True}, ttl=60)
-
-    value = get_cache("test_cache")
-
-    info("Cache test value: {}".format(value))
 
 
 if __name__ == "__main__":
