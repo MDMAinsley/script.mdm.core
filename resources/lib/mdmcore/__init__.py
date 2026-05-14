549a931aaec552d4d02027d0250ed61dfcfaf20e
MDMAinsley <lewis.james.short@gmail.com>
2026-05-14 17:40:24 +0100

    v0.1.2

--- resources/lib/mdmcore/__init__.py
+++ resources/lib/mdmcore/__init__.py
@@ -2,10 +2,33 @@
 
 from .bootstrap import initialise_core
 
-__version__ = "0.1.1"
+from . import logger
+from . import settings
+from . import paths
+from . import files
+from . import cache
+from . import network
+from . import device
+from . import jsonrpc
+from . import monitor
+from . import integrations
+from . import timeutils
+
+__version__ = "0.1.2"
 __author__ = "MDMAinsley"
 __package_name__ = "mdmcore"
 
 __all__ = [
     "initialise_core",
+    "logger",
+    "settings",
+    "paths",
+    "files",
+    "cache",
+    "network",
+    "device",
+    "jsonrpc",
+    "monitor",
+    "integrations",
+    "timeutils",
 ]
